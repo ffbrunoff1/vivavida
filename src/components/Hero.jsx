658833abc65d4605c52dbdff1eff0ba3f790e9e6
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-08-12 17:57:40 -0300

    Atualizar arquivos do projeto - 2025-08-12T20:57:40.479Z

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -98,8 +98,7 @@ export default function Hero() {
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
-                <Calendar className="w-5 h-5 mr-2" />
-                Agendar Consulta
+                Marque sua Consulta
                 <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
               </motion.button>
 
